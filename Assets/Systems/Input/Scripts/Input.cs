3bed87f758d9bc861b8c88a616fed75b4c47f477
ahanthel <Ahanthel@gmail.com>
2022-01-21 15:21:12 +0100

    q

---
+++ Assets/Systems/Input/Scripts/Input.cs
@@ -0,0 +1,48 @@
+// ----------------------------------------------------------------------------
+// Simulering av Dykkerpost
+// Bachelor Oppgave våren 2022
+// 
+// Aahed Diyab, Olav Pete
+// ----------------------------------------------------------------------------
+
+using Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Input
+{
+    public class Input : MonoBehaviour
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Unity Methods
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+        #endregion
+        
+    } 
+}
