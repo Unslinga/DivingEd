b9be7945574adf8b0a74a3fefe4a9c6147b462b8
Olav Pete <olav@engineworks.no>
2022-01-21 11:31:27 +0100

    changes

---
+++ Assets/Systems/Gauge/Scripts/Gauge.cs
@@ -0,0 +1,47 @@
+// ----------------------------------------------------------------------------
+// Simulering av Dykkerpost
+// Bachelor Oppgave våren 2022
+// 
+// Aahed Diyab, Olav Pete
+// ----------------------------------------------------------------------------
+
+using Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DEFAULT_REPLACE
+{
+    public class Gauge : MonoBehaviour
+    {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Unity Methods
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+        #endregion
+        
+    } 
+}
