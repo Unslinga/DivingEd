d9c5efb314c4195aed0d0361e51a4f286d68ec4a
ahanthel <Ahanthel@gmail.com>
2022-03-24 13:34:49 +0100

    asds

---
+++ Assets/Systems/Gauge/Scripts/Graph/Valve.cs
@@ -0,0 +1,57 @@
+// ----------------------------------------------------------------------------
+// Simulering av Dykkerpost
+// Bachelor Oppgave våren 2022
+// 
+// Aahed Diyab, Olav Pete
+// ----------------------------------------------------------------------------
+
+using Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Referanse gameobject
+//active/inactive
+
+namespace Valve
+{
+    public class Valve : MonoBehaviour
+    {
+        #region Fields & Properties
+        [field:SerializeField]
+        public InputEventSet Inputs { get; set; }
+        //[field: SerializeField]
+        //GameObject gameObject;
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+        public void ToggleMouseDrag(object data)
+        {
+            Debug.Log("sadasdas");
+            
+        }
+        #endregion
+
+        #region Unity Methods
+
+        void Start()
+        {
+            Inputs["LeftClick"].CreateListener(gameObject, ToggleMouseDrag);
+        }
+
+        void Update()
+        {
+        
+        }
+
+        #endregion
+    }
+}
+
+// Inputcontroller
+// bool active/inactive
